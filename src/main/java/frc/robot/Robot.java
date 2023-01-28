53fb5d5ad7766ddef62adeb5eb3332505cdc8f27
Austin Spencer <mamichalske@yahoo.com>
2023-01-28 14:18:49 -0500

    We got SOMETHING running
    warning code may troll, code made by anonomous hacker (me) bewware  !!!! !11!!!1
    
    sorry for using your account Marcus

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,23 +4,88 @@
 
 package frc.robot;
 
+import com.kauailabs.navx.frc.AHRS;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.Counter;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends TimedRobot {
+  private AHRS ahrs;
+  private Joystick stick = new Joystick(0);
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
+  private AddressableLED m_led;
+  AddressableLEDBuffer m_ledBuffer;
+  Counter systemCount;
+
+  public final CANSparkMax m_move = new CANSparkMax(14, MotorType.kBrushless);
+  private RelativeEncoder m_moveEncoder = m_move.getEncoder();
 
   @Override
   public void robotInit() {
     m_robotContainer = new RobotContainer();
+    systemCount = new Counter();
+    
+    try {
+      /***********************************************************************
+       * navX-MXP: - Communication via RoboRIO MXP (SPI, I2C) and USB. - See
+       * http://navx-mxp.kauailabs.com/guidance/selecting-an-interface.
+       * 
+       * navX-Micro: - Communication via I2C (RoboRIO MXP or Onboard) and USB. - See
+       * http://navx-micro.kauailabs.com/guidance/selecting-an-interface.
+       * 
+       * VMX-pi: - Communication via USB. - See
+       * https://vmx-pi.kauailabs.com/installation/roborio-installation/
+       * 
+       * Multiple navX-model devices on a single robot are supported.
+       ************************************************************************/
+      ahrs = new AHRS(SPI.Port.kMXP);
+      // ahrs = new AHRS(SerialPort.Port.kUSB1);
+      ahrs.enableLogging(true);
+
+    } catch (RuntimeException ex) {
+      DriverStation.reportError("Error instantiating navX MXP:  " + ex.getMessage(), true);
+    }
+
+    m_led = new AddressableLED(0);
+    System.out.println("hello");
+    // Reuse buffer
+    // Default to a length of 60, start empty output
+    // Length is expensive to set, so only set it once, then just update data
+    m_ledBuffer = new AddressableLEDBuffer(60);
+    m_led.setLength(m_ledBuffer.getLength());
+
+    // Set the data
+    m_led.setData(m_ledBuffer);
+    m_led.start();
+
+    // Motor related stuff
+    m_move.restoreFactoryDefaults();  
+    m_move.setSmartCurrentLimit(40, 40);    
+    m_move.setIdleMode(IdleMode.kBrake);
   }
 
   @Override
   public void robotPeriodic() {
     CommandScheduler.getInstance().run();
+    // Fill the buffer with a rainbow
+
+    rainbow((int)ahrs.getFusedHeading());
+    // Set the LEDs
+    m_led.setData(m_ledBuffer);
   }
 
   @Override
@@ -55,7 +120,107 @@ public class Robot extends TimedRobot {
   }
 
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    boolean zero_yaw_pressed = stick.getTrigger();
+    if (zero_yaw_pressed) {
+      ahrs.zeroYaw();
+      System.out.print("trolled");
+    }
+
+    if (stick.getRawButton(1)) {
+      // Drive motor for test bed
+      m_move.set(1);
+
+    }
+    else{
+      m_move.set(0);
+    }
+
+    /* Display 6-axis Processed Angle Data */
+    SmartDashboard.putBoolean("IMU_Connected", ahrs.isConnected());
+    SmartDashboard.putBoolean("IMU_IsCalibrating", ahrs.isCalibrating());
+    SmartDashboard.putNumber("IMU_Yaw", ahrs.getYaw());
+    SmartDashboard.putNumber("IMU_Pitch", ahrs.getPitch());
+    SmartDashboard.putNumber("IMU_Roll", ahrs.getRoll());
+
+    /* Testing to see if roborio is getting input from controller */
+    SmartDashboard.putBoolean("A button pressed", stick.getRawButton(1));
+
+    /* Display tilt-corrected, Magnetometer-based heading (requires */
+    /* magnetometer calibration to be useful) */
+
+    SmartDashboard.putNumber("IMU_CompassHeading", ahrs.getCompassHeading());
+
+    /* Display 9-axis Heading (requires magnetometer calibration to be useful) */
+    SmartDashboard.putNumber("IMU_FusedHeading", ahrs.getFusedHeading());
+
+    /* These functions are compatible w/the WPI Gyro Class, providing a simple */
+    /* path for upgrading from the Kit-of-Parts gyro to the navx MXP */
+
+    SmartDashboard.putNumber("IMU_TotalYaw", ahrs.getAngle());
+    SmartDashboard.putNumber("IMU_YawRateDPS", ahrs.getRate());
+
+    /* Display Processed Acceleration Data (Linear Acceleration, Motion Detect) */
+
+    SmartDashboard.putNumber("IMU_Accel_X", ahrs.getWorldLinearAccelX());
+    SmartDashboard.putNumber("IMU_Accel_Y", ahrs.getWorldLinearAccelY());
+    SmartDashboard.putBoolean("IMU_IsMoving", ahrs.isMoving());
+    SmartDashboard.putBoolean("IMU_IsRotating", ahrs.isRotating());
+
+    /* Display estimates of velocity/displacement. Note that these values are */
+    /* not expected to be accurate enough for estimating robot position on a */
+    /* FIRST FRC Robotics Field, due to accelerometer noise and the compounding */
+    /* of these errors due to single (velocity) integration and especially */
+    /* double (displacement) integration. */
+
+    SmartDashboard.putNumber("Velocity_X", ahrs.getVelocityX());
+    SmartDashboard.putNumber("Velocity_Y", ahrs.getVelocityY());
+    SmartDashboard.putNumber("Displacement_X", ahrs.getDisplacementX());
+    SmartDashboard.putNumber("Displacement_Y", ahrs.getDisplacementY());
+
+    /* Display Raw Gyro/Accelerometer/Magnetometer Values */
+    /* NOTE: These values are not normally necessary, but are made available */
+    /* for advanced users. Before using this data, please consider whether */
+    /* the processed data (see above) will suit your needs. */
+
+    SmartDashboard.putNumber("RawGyro_X", ahrs.getRawGyroX());
+    SmartDashboard.putNumber("RawGyro_Y", ahrs.getRawGyroY());
+    SmartDashboard.putNumber("RawGyro_Z", ahrs.getRawGyroZ());
+    SmartDashboard.putNumber("RawAccel_X", ahrs.getRawAccelX());
+    SmartDashboard.putNumber("RawAccel_Y", ahrs.getRawAccelY());
+    SmartDashboard.putNumber("RawAccel_Z", ahrs.getRawAccelZ());
+    SmartDashboard.putNumber("RawMag_X", ahrs.getRawMagX());
+    SmartDashboard.putNumber("RawMag_Y", ahrs.getRawMagY());
+    SmartDashboard.putNumber("RawMag_Z", ahrs.getRawMagZ());
+    SmartDashboard.putNumber("IMU_Temp_C", ahrs.getTempC());
+    SmartDashboard.putNumber("IMU_Timestamp", ahrs.getLastSensorTimestamp());
+
+    /* Omnimount Yaw Axis Information */
+    /* For more info, see http://navx-mxp.kauailabs.com/installation/omnimount */
+    AHRS.BoardYawAxis yaw_axis = ahrs.getBoardYawAxis();
+    SmartDashboard.putString("YawAxisDirection", yaw_axis.up ? "Up" : "Down");
+    SmartDashboard.putNumber("YawAxis", yaw_axis.board_axis.getValue());
+
+    /* Sensor Board Information */
+    SmartDashboard.putString("FirmwareVersion", ahrs.getFirmwareVersion());
+
+    /* Quaternion Data */
+    /* Quaternions are fascinating, and are the most compact representation of */
+    /* orientation data. All of the Yaw, Pitch and Roll Values can be derived */
+    /* from the Quaternions. If interested in motion processing, knowledge of */
+    /* Quaternions is highly recommended. */
+    SmartDashboard.putNumber("QuaternionW", ahrs.getQuaternionW());
+    SmartDashboard.putNumber("QuaternionX", ahrs.getQuaternionX());
+    SmartDashboard.putNumber("QuaternionY", ahrs.getQuaternionY());
+    SmartDashboard.putNumber("QuaternionZ", ahrs.getQuaternionZ());
+
+    /* Connectivity Debugging Support */
+    SmartDashboard.putNumber("IMU_Byte_Count", ahrs.getByteCount());
+    SmartDashboard.putNumber("IMU_Update_Count", ahrs.getUpdateCount());
+    SmartDashboard.putNumber("IMU Rate", ahrs.getRate());
+
+    SmartDashboard.putNumber("Tick Count", systemCount.get());
+  }
 
   @Override
   public void teleopExit() {}
@@ -70,4 +235,19 @@ public class Robot extends TimedRobot {
 
   @Override
   public void testExit() {}
+
+  private void rainbow(int m_rainbowFirstPixelHue) {
+    // For every pixel
+    for (var i = 0; i < m_ledBuffer.getLength(); i++) {
+      // Calculate the hue - hue is easier for rainbows because the color
+      // shape is a circle so only one value needs to precess
+      final int hue = (m_rainbowFirstPixelHue + (i * 180 / m_ledBuffer.getLength())) % 180;
+      // Set the value
+      m_ledBuffer.setHSV(i, hue, 255, 128);
+    }
+    // Increase by to make the rainbow "move"
+    m_rainbowFirstPixelHue += 3;
+    // Check bounds
+    m_rainbowFirstPixelHue %= 180;
+  }
 }
